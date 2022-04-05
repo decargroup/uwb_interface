230ad26f21386be4df62cb4d0f2093032db87dea
Charles Cossette <charles.cossette@mail.mcgill.ca>
2022-04-05 12:46:46 -0400

    Added new packer object and test. Needs to be integrated into UwbModule

---
+++ pyuwb/packing.py
@@ -0,0 +1,138 @@
+import struct
+from typing import Any, List
+from abc import ABC, abstractmethod
+encoding = "utf-8"
+
+
+class Field(ABC):
+    """
+    An abstract Field is essentially a namespace for two specific methods:
+
+     - a `pack` method that takes a specific variable type and returns a 
+       representation in bytes 
+     - an `unpack` method that takes an array of bytes and returns a variable 
+       of that type.
+
+    In the unpack method, the index of the next key character will also be 
+    supplied. A key character is either a field seperator or a message 
+    terminator. The implementation of the unpack method can therefore use the 
+    location of the key character to detect how many bytes to read. As a 
+    second output argument, it is mandatory to provide the index of the first
+    byte of the following field in the message. 
+    
+    """
+    @staticmethod
+    @abstractmethod
+    def pack(value: Any) -> bytes:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def unpack(msg: bytes, next_key_idx: int) -> Any:
+        # return value, next_field_idx
+        pass
+
+
+class IntField:
+    @staticmethod
+    def pack(value: int) -> bytes:
+        return str(value).encode(encoding)
+
+    @staticmethod
+    def unpack(msg: bytes, next_key_idx: int) -> int:
+        return int(msg[:next_key_idx].decode(encoding)), next_key_idx+1
+
+
+class BoolField:
+    @staticmethod
+    def pack(value: bool) -> bytes:
+        return str(int(value)).encode(encoding)
+
+    @staticmethod
+    def unpack(msg: bytes, next_key_idx: int) -> bool:
+        if next_key_idx != 1:
+            raise RuntimeError("Bool field is more than 1 byte..")
+
+        return bool(msg[:next_key_idx].decode(encoding)), next_key_idx+1
+
+
+class StringField:
+    @staticmethod
+    def pack(value: str) -> bytes:
+        return value.encode(encoding)
+
+    @staticmethod
+    def unpack(msg: bytes, next_key_idx: int) -> str:
+        return msg[:next_key_idx].decode(encoding), next_key_idx + 1
+
+
+class FloatField:
+    @staticmethod
+    def pack(value: float) -> bytes:
+        return struct.pack("<f", value)
+
+    @staticmethod
+    def unpack(msg: bytes, next_key_idx: int) -> float:
+        return float(msg[:next_key_idx].decode(encoding)), next_key_idx+1
+
+
+class ByteField:
+    @staticmethod
+    def pack(value: bytes) -> bytes:
+        num_bytes = len(value)
+        return struct.pack("<H", num_bytes) + value
+
+    @staticmethod
+    def unpack(msg: bytes, next_key_idx: int) -> bytes:
+        fieldlen = struct.unpack(
+            "<H", msg[:2]
+        )
+        
+        return msg[2 : 2 + fieldlen[0]], 2 + fieldlen[0] + 1
+    
+
+class Packer:
+    def __init__(self, seperator: str = "|", terminator: str = "\r"):
+        self._sep = seperator.encode(encoding)
+        self._eol = terminator.encode(encoding)
+
+    def pack(self, field_values: List[Any], field_types: List[Field]) -> bytes:
+        msg = b""
+        for i, val in enumerate(field_values):
+            msg += self._sep + field_types[i].pack(val)
+        msg += self._eol
+
+        return msg[1:] # Dont include initial seperator
+
+    def unpack(self, msg: bytes, field_types: List[Field]):
+
+        # If no expected fields, return empty.
+        results = [] 
+        if field_types is None or len(field_types) == 0:
+            return results
+
+        for field in field_types:
+
+            # Find the "soonest" key character (seperator or terminator)
+            next_sep = msg.find(self._sep)
+            next_eol = msg.find(self._eol)
+            if next_sep == -1 and next_eol == -1:
+                raise RuntimeError(
+                    "No seperator or terminator detected in received message."
+                )
+            elif next_sep == -1:
+                next_key_idx = next_eol
+            elif next_eol == -1:
+                next_key_idx = next_sep
+            else:
+                next_key_idx = min(next_sep, next_eol)
+
+            # Unpack the value
+            value, next_field_idx = field.unpack(msg, next_key_idx)
+            results.append(value)
+
+            # Move through the message to the next field.
+            msg = msg[next_field_idx:]
+
+        return results 
+    
---
+++ tests/test_packing.py
@@ -0,0 +1,38 @@
+from pyuwb.packing import * 
+import msgpack
+
+def test_all_types():
+    test_dict = {"a": 3.14159, "b": [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]}
+    test_bytes = msgpack.packb(test_dict)
+    test_values = [
+        123456,
+        "the test string",
+        True,
+        1.234567891011e-8,
+        test_bytes,
+    ]
+    test_types = [
+        IntField,
+        StringField,
+        BoolField,
+        FloatField,
+        ByteField
+    ]
+
+    test_unpack = (
+        b"123456|the test string|1|1.234567891011e-8|"
+        b"G\x00\x82\xa1a\xcb@\t!\xf9\xf0\x1b\x86n\xa1b\x92\x93\xcb?\xf0\x00\x00\x00"
+        b"\x00\x00\x00\xcb@\x00\x00\x00\x00\x00\x00\x00\xcb@\x08\x00\x00\x00\x00"
+        b"\x00\x00\x93\xcb@\x10\x00\x00\x00\x00\x00\x00\xcb@\x14\x00\x00\x00\x00"
+        b"\x00\x00\xcb@\x18\x00\x00\x00\x00\x00\x00\r"
+    )
+
+    packer = Packer()
+    msg = packer.pack(test_values, test_types)
+    values_unpacked = packer.unpack(test_unpack, test_types)
+    assert values_unpacked == test_values 
+    assert msg.split(packer._sep)[:3]  == test_unpack.split(packer._sep)[:3] 
+
+if __name__ == "__main__":
+    test_all_types()
+
